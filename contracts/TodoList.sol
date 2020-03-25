444fc0bf551fb1d0612ae5193ed93a967052617f
Gianfranco <gianfrancobtl@gmail.com>
2020-03-25 16:54:32 -0300

    added the function to create a task

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -21,6 +21,13 @@ contract TodoList {
     // we get returned all the tasks in the list.
     mapping(uint => Task) public tasks;
 
+    // event we can subscribe or unsubscribe
+    event TaskCreated (
+        uint id,
+        string content,
+        bool completed
+    );
+
     // default task for the todo-list:
     constructor() public {
         createTask('Read Sherlock Holmes for school.');
@@ -31,5 +38,6 @@ contract TodoList {
     function createTask(string memory _content) public {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
     }
 }

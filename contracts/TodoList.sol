14d7ee846dded5accd51cb3f088e7ce56e75dbe2
Gianfranco <gianfrancobtl@gmail.com>
2020-03-25 14:51:34 -0300

    developing client side front-end application

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,6 +4,32 @@ pragma solidity ^0.5.0;
 contract TodoList {
     // keep track of the n of tasks
     // state (uint) var: written to the blockchain
+    // unsigned integer: integer that can't be negative
     // public: the value is readable
     uint public taskCount = 0;
+
+    // own data types (structs)
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+    // mapping =~ hash/associative array. K/V storaged.
+    // key: uint(ID); value: the task
+    // =~ DataBase
+    // todoList.tasks ::: when it called,
+    // we get returned all the tasks in the list.
+    mapping(uint => Task) public tasks;
+
+    // default task for the todo-list:
+    constructor() public {
+        createTask('Read Sherlock Holmes for school.');
+    }
+
+    // function: create a new task.
+    // put the struct into the mapping.
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }

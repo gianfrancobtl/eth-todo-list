ec56d83ce0ba18a3f8a38644478a3505058ef24a
Gian <gianfrancobtl@gmail.com>
2021-10-26 15:32:03 -0300

    Retrieving eth address perfetly

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 contract TodoList {
     // # tasks initialized in 0.
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Revisar el correo.");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
